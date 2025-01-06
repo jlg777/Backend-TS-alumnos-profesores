a73bffc8ab3777f974c5d13da46c9273331b6eb9
= <=>
2025-01-06 15:25:30 -0300

    se activa el middleware y cors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import morgan from 'morgan';
 
 const app = express();
 const PORT = 3000;
+// Activar el middleware Morgan
+app.use(morgan('dev'));
+// Habilitar CORS para todas las rutas
+app.use(cors());
+
+// Rutas
+app.get('/', (req, res) => {
+    res.send('¡Hola Mundo!');
+  });
 
 app.listen(PORT, () => {
     console.log(`escuchando en el puerto http://localhost:${PORT}`)
