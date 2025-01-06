6f9fd35f48cd07d483ff33b6e6cf19ed72dd0ccb
= <=>
2025-01-06 15:22:05 -0300

    se crea el servidor

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hola mundo")
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`escuchando en el puerto http://localhost:${PORT}`)
+})
