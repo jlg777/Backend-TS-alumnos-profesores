13ac8e469c8cf193be6b4ffa2e129349f3be39b8
= <=>
2025-01-07 12:10:37 -0300

    controllers ESTUDIANTES - rutas

--- src/routes/estudiantesRoutes.ts
+++ src/routes/estudiantesRoutes.ts
@@ -6,9 +6,7 @@ estudiantesRoutes.get('/', estudiantesControllers.consultar);
 estudiantesRoutes.post('/', estudiantesControllers.ingresar);
 
 estudiantesRoutes.get('/:id', estudiantesControllers.consultardetalle);
-
 estudiantesRoutes.put('/:id', estudiantesControllers.actualizarestudiante);
-
 estudiantesRoutes.delete('/:id', estudiantesControllers.borrarestudiante);
 
 export default estudiantesRoutes;
