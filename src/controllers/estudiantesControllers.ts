fefddd728c8a69cb3ab65dbefbfb7ceb566f3f82
= <=>
2025-01-07 12:01:47 -0300

    controllers ESTUDIANTES - rutas

--- src/controllers/estudiantesControllers.ts
+++ src/controllers/estudiantesControllers.ts
@@ -18,5 +18,67 @@ class EstudiantesControllers {
         }
     }
 
+    ingresar(req: Request, res: Response) {
+        try {
+            res.send(' Ingreso Estudiantes Controllers')
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los estudiantes:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
+
+    consultardetalle(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            res.send(`Estudiante Controllers ${id}`)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los estudiantes:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
+
+    actualizarestudiante(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            res.send(`Modificar Estudiante Controllers ${id}`)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los estudiantes:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
+
+    borrarestudiante(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            res.send(`Borrar Estudiante Controllers ${id}`)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los estudiantes:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
 }
 export default new EstudiantesControllers;
--- src/routes/estudiantesRoutes.ts
+++ src/routes/estudiantesRoutes.ts
@@ -3,5 +3,12 @@ import estudiantesControllers from "../controllers/estudiantesControllers";
 const estudiantesRoutes = express.Router()
 
 estudiantesRoutes.get('/', estudiantesControllers.consultar);
+estudiantesRoutes.post('/', estudiantesControllers.ingresar);
+
+estudiantesRoutes.get('/:id', estudiantesControllers.consultardetalle);
+
+estudiantesRoutes.put('/:id', estudiantesControllers.actualizarestudiante);
+
+estudiantesRoutes.delete('/:id', estudiantesControllers.borrarestudiante);
 
 export default estudiantesRoutes;
