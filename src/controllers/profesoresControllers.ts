74a558f80c4a53b66210b8cbf6dbcb04b715097b
= <=>
2025-01-07 13:29:47 -0300

    controllers PROFESORES - rutas

--- src/controllers/profesoresControllers.ts
+++ src/controllers/profesoresControllers.ts
@@ -18,5 +18,68 @@ class ProfesoresControllers {
         }
     }
 
+    ingresar(req: Request, res: Response) {
+        try {
+            res.send('Ingresar Profesores Controllers')
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los profesores:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
+
+    consultardetalle(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            res.send(`Consultar profesor Controllers ${id}`)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los profesores:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
+
+    actualizarprofesor(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            res.send(`Actualizar Profesor Controllers ${id}`)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los profesores:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
+
+    borrarprofesor(req: Request, res: Response) {
+        const { id } = req.params;
+        try {
+            res.send(`Borrar Profesor Controllers ${id}`)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error('Error al consultar los profesores:', error);
+                res.status(500).send(error.message);
+            } else {
+                // En caso de que el error no sea una instancia de Error
+                console.error('Error desconocido:', error);
+                res.status(500).send('Error desconocido');
+            }
+        }
+    }
+
 }
 export default new ProfesoresControllers;
--- src/routes/profesoresRoutes.ts
+++ src/routes/profesoresRoutes.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import profesoresControllets from "../controllers/profesoresControllers";
+import profesoresControllers from "../controllers/profesoresControllers";
 const profesoresRoutes = express.Router()
 
 profesoresRoutes.get('/',profesoresControllets.consultar);
+profesoresRoutes.post('/',profesoresControllets.ingresar);
+
+profesoresRoutes.get('/:id', profesoresControllers.consultardetalle);
+profesoresRoutes.put('/:id', profesoresControllers.actualizarprofesor);
+profesoresRoutes.delete('/:id', profesoresControllers.borrarprofesor);
 
 export default profesoresRoutes;
